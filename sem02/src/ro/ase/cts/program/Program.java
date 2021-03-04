92170364144aee8ef419debaab67b485a46b0f25
tddrrr <teodora.birliba@gmail.com>
2021-03-04 10:22:49 +0200

    refactor

--- sem02/src/ro/ase/cts/program/Program.java
+++ sem02/src/ro/ase/cts/program/Program.java
@@ -4,14 +4,14 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.readers.AngajatReader;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
